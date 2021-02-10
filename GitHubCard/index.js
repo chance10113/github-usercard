4b3e7a6a0222f455fd3a7bffe3f28567c64cf254
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-09 18:32:38 -0600

    rebuilt app

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,42 +1,26 @@
-//const { default: Axios } = require("axios");
-import axios from "axios";
+import axios from "axios"
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-const entry = document.querySelector('.cards')
+
+const entryPoint = document.querySelector('.cards')
 
 axios
-.get('https://api.github.com/users/chance10113')
-.then(res => {
-  const gitInfo = res.data
-  const newCard = cardMaker(gitInfo)
-  entry.appendChild(newCard)
-  console.log('this is a thing, you know it when you see it', gitInfo)
-})
-.catch(beef => {
-  console.log(beef)
-})
-// .then(res) => {
-//   // future code for when the data actually arrives
-//   // freedom to assume that the data is here
-// const stuff = res.data;
-// const newCard = cardMaker(res.data)
-//       entry.appendChild(card)
-//       console.log("2. here is the future data", res);
-// })
-// .catch((drama) => {
-//   // handle the drama
-//   console.log(drama);
-// });
+  .get('https://api.github.com/users/chance10113')
+  .then((res) => {
+    // console.log('res', res)
+    // console.log('res.data', res.data)
+    const gitInfo = res.data
+    const newCard = cardMaker(gitInfo)
+    entryPoint.appendChild(newCard)
+    console.log('gitInfo', gitInfo)
 
-
-// .then(res => {
-//   const gitInfo = res.data
-//   const gitCard = gitCardMaker(res.data)
-//   entryPoint.appendChild(gitCard)
-// })
+  })
+  .catch((error) => {
+    console.log('error', error)
+  })
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -60,32 +44,35 @@ axios
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
-//1like2learn
+
+// 'kristapants',
+// 'loustevenshere',
+// '1like2learn',
+// 'bigknell',
+// 'dustinmyers'
 let followersArray = [];
+
 axios
 .get('https://api.github.com/users/chance10113/followers')
 .then(res =>
-{ followersArray = res.data
+{followersArray = res.data
   console.log('Follow stuff', followersArray)
   followersArray.forEach(gitInfo => {
     axios
     .get(gitInfo.url)
     .then(response => {
       const newCard = cardMaker(response.data)
-        entry.appendChild(newCard)
-        console.log('Steve', response.data) 
+        entryPoint.appendChild(newCard)
+        console.log('response.data', response.data) 
     })
   })
 }  )
-.catch((drama) => {
-  console.log(drama)
+.catch((error) => {
+  console.log(error)
 })
 
-// followersArray.forEach(gitInfo => {
-//   const newCard = cardMaker(gitInfo)
-//     entry.appendChild(newCard)
-//     console.log('Steve', gitInfo)
-// })
+//Write this using a string literal, so that it maps/forEachs through the array
+
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -106,49 +93,48 @@ axios
     </div>
 */
 
-function cardMaker(gitInfo){
-  //creating elements
-  const card = document.createElement('div');
-  const img = document.createElement('img');
-  const cardInfo = document.createElement('div');
-  const name = document.createElement('h3');
-  const userName =document.createElement('p')
-  const location = document.createElement('p');
-  const profile = document.createElement('p');
-  const followers = document.createElement('p');
-  const following = document.createElement('p');
-  const bio = document.createElement('p');
-  const anchor = document.createElement('a');
-//adding classes
-card.classList.add('card');
-cardInfo.classList.add('card-info');
-name.classList.add('name');
-userName.classList.add('username');
-//stitching together
-card.appendChild(img);
-card.appendChild(cardInfo);
-cardInfo.appendChild(name);
-cardInfo.appendChild(userName);
-cardInfo.appendChild(location);
-cardInfo.appendChild(profile);
-cardInfo.appendChild(followers);
-cardInfo.appendChild(following);
-cardInfo.appendChild(bio);
-profile.appendChild(anchor)
-//Content
-img.src = gitInfo.avatar_url
-name.textContent = gitInfo.name;
-userName.textContent = gitInfo.login
-location.textContent = `Location: ${gitInfo.location}`
-anchor.textContent = ` Profile: ${gitInfo.url}`;
-anchor.href = gitInfo.html_url;
-followers.textContent = `Followers: ${gitInfo.followers}`;
-following.textContent = `Following: ${gitInfo.following}`;
-bio.textContent = `Bio: ${gitInfo.bio}`;
-return card
+function cardMaker ( gitInfo ) {
+// Creating Elements
+  const card = document.createElement('div')
+  const img = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const userName = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const anchor = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+// Adding Class Names
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  userName.classList.add('username')
+// Stitching it all together
+  card.appendChild(img)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(userName)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  profile.appendChild(anchor)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+// Adding Content
+  img.src = gitInfo.avatar_url
+  name.textContent = gitInfo.name
+  userName.textContent = gitInfo.username
+  location.textContent = `Location: ${gitInfo.location}`
+  anchor.textContent = `Profile: ${gitInfo.url}`
+  anchor.href = gitInfo.html_url
+  followers.textContent = `Followers: ${gitInfo.followers}`
+  following.textContent = `Following: ${gitInfo.following}`
+  bio.textContent = `Bio: ${gitInfo.bio}`
+
+  return card;
 }
-
-
 /*
   List of LS Instructors Github username's:
     tetondan
